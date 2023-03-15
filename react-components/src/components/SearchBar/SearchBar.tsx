58f88202ace11d54bc590b93bdfaa3e4b4738703
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-03-15 16:49:51 +0600

    feat: add state for SerchBar

--- react-components/src/components/SearchBar/SearchBar.tsx
+++ react-components/src/components/SearchBar/SearchBar.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { InputState } from './inputState.interface';
 import classes from './SearchBar.module.css';
 
-class SearchBar extends React.Component {
+class SearchBar extends React.Component<Record<string, never>, InputState> {
+  constructor(props: Record<string, never>) {
+    super(props);
+    this.state = {
+      text: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e: React.FormEvent<HTMLInputElement>): void {
+    this.setState({ text: e.currentTarget.value });
+  }
+
   render(): React.ReactNode {
     return (
       <form className={classes.form}>
-        <input className={classes.input} type="text" placeholder="Type text here" />
+        <input
+          className={classes.input}
+          type="text"
+          value={this.state.text}
+          onChange={this.handleChange}
+          placeholder="Type text here"
+        />
         <button className={classes.button} type="submit">
           Search
         </button>
