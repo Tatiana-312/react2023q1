33fc5593a65bcbd0afcf96d6afe83e1bdf821c05
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-03-16 21:08:34 +0600

    feat: setup vitest and add test for card

---
+++ react-components/src/components/Card/Card.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { it, describe, expect } from 'vitest';
+import Card from './Card';
+
+const testData = {
+    "id": 1,
+    "imgUrl": "https://m.media-amazon.com/images/P/0553277537.01._SCLZZZZZZZ_SX500_.jpg",
+    "name": "Dandelion Wine",
+    "author": "Ray Bredbury",
+    "genre": "Novel"
+}
+
+describe('Card component', () => {
+    it('Card renders', () => {
+        render(
+            <Card key={testData.id} {...testData} />
+        )
+
+        expect(screen.getByRole('img')).toBeInTheDocument();
+        expect(screen.getByRole('heading', { level: 2 })).toBeInTheDocument();
+        expect(screen.getByRole('heading', { level: 3 })).toBeInTheDocument();
+        expect(screen.getByRole('button')).toBeInTheDocument();
+        expect(screen.getByText('Novel')).toBeInTheDocument();
+    });
+});
