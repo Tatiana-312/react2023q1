476f8c996117e3076525f95203bdaaea985a5a03
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-03-16 13:38:23 +0600

    feat: add pageTitle

--- react-components/src/components/Header/Header.tsx
+++ react-components/src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import NavBar from '../NavBar/NavBar';
 import React from 'react';
 import classes from './Header.module.css';
+import PageTitle from '../PageTitle/PageTitle';
 
 class Header extends React.Component {
   render() {
@@ -8,6 +9,7 @@ class Header extends React.Component {
       <div className={classes.header}>
         <div className={classes.header_navbar_container}>
           <NavBar />
+          <PageTitle {...{pageTitle: 'unknown'}}/>
         </div>
       </div>
     );
---
+++ react-components/src/components/PageTitle/PageTitle.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { PageTitleProps } from './pageTitle.interface';
+import classes from './PageTitle.module.css';
+
+class PageTitle extends React.Component<PageTitleProps> {
+  constructor(props: PageTitleProps) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1 className={classes.title}>{this.props.pageTitle} page</h1>
+      </div>
+    );
+  }
+}
+
+export default PageTitle;
