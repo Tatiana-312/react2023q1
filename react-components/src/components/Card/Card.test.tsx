e3f917c631e10b27f90c1c56ab2bbaafda01072f
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-03-16 21:12:40 +0600

    feat: add configs for coverage

--- react-components/src/components/Card/Card.test.tsx
+++ react-components/src/components/Card/Card.test.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { it, describe, expect } from 'vitest';
 import Card from './Card';
 
 const testData = {
