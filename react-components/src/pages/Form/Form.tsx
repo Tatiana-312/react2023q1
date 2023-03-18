696e25a1ee4c1d4331aebe4ab207d823881e85ed
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-03-18 16:05:30 +0600

    feat: add Form component

---
+++ react-components/src/pages/Form/Form.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Form.module.css';
+
+class Form extends React.Component {
+  render(): React.ReactNode {
+    return <div className={classes.form__container}>Form</div>;
+  }
+}
+
+export default Form;
