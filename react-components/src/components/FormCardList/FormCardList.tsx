721bcb0dde0e08cc552e954816bd02283dec8e48
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-03-28 20:44:51 +0600

    feat: add hooks for FormCardList

--- react-components/src/components/FormCardList/FormCardList.tsx
+++ react-components/src/components/FormCardList/FormCardList.tsx
@@ -4,20 +4,14 @@ import FormCard from '../FormCard/FormCard';
 import classes from './FormCardList.module.css';
 import { FormCardListProps } from './formCardListProps.interface';
 
-class FormCardList extends React.Component<FormCardListProps> {
-  constructor(props: FormCardListProps) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className={classes.cardList__container}>
-        {this.props.cards.map((item: CardData, index: number) => {
-          return <FormCard key={index} {...item} />;
-        })}
-      </div>
-    );
-  }
-}
+const FormCardList: React.FC<FormCardListProps> = ({ cards }) => {
+  return (
+    <div className={classes.cardList__container}>
+      {cards.map((item: CardData, index: number) => {
+        return <FormCard key={index} {...item} />;
+      })}
+    </div>
+  );
+};
 
 export default FormCardList;
