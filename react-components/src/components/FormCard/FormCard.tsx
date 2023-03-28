65e735e44ce04efeeb89e47a9bdabe74a2dfec7c
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-03-28 20:54:44 +0600

    feat: replace FormCard class with function

--- react-components/src/components/FormCard/FormCard.tsx
+++ react-components/src/components/FormCard/FormCard.tsx
@@ -2,32 +2,27 @@ import React from 'react';
 import { CardData } from '../Form/cardData.interface';
 import classes from './FormCard.module.css';
 
-class FormCard extends React.Component<CardData> {
-  constructor(props: CardData) {
-    super(props);
-  }
-  render() {
-    return (
-      <div data-testid="form-card" className={classes.card}>
-        <p>
-          <span>Name: </span>
-          {this.props.name}
-        </p>
-        <p>
-          <span>Date: </span>
-          {this.props.date}
-        </p>
-        <p>
-          <span>Country: </span>
-          {this.props.country}
-        </p>
-        <img className={classes.card__img} src={this.props.file} alt="book cover" />
-        <p>
-          <span>Payment method: </span>
-          {this.props.payment}
-        </p>
-      </div>
-    );
-  }
-}
+const FormCard: React.FC<CardData> = (props) => {
+  return (
+    <div data-testid="form-card" className={classes.card}>
+      <p>
+        <span>Name: </span>
+        {props.name}
+      </p>
+      <p>
+        <span>Date: </span>
+        {props.date}
+      </p>
+      <p>
+        <span>Country: </span>
+        {props.country}
+      </p>
+      <img className={classes.card__img} src={props.file} alt="book cover" />
+      <p>
+        <span>Payment method: </span>
+        {props.payment}
+      </p>
+    </div>
+  );
+};
 export default FormCard;
