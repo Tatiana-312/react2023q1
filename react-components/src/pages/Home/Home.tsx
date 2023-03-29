02140663e3b45c66a5c03b11c303238c70165da7
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-03-29 18:54:21 +0600

    feat: rewrite Home class component with function

--- react-components/src/pages/Home/Home.tsx
+++ react-components/src/pages/Home/Home.tsx
@@ -3,16 +3,14 @@ import React from 'react';
 import Title from '../../components/Title/Title';
 import CardList from '../../components/CardList/CardList';
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div data-testid="home-page">
-        <Title {...{ title: 'Book Shop' }} />
-        <SearchBar />
-        <CardList />
-      </div>
-    );
-  }
-}
+const Home: React.FC = () => {
+  return (
+    <div data-testid="home-page">
+      <Title {...{ title: 'Book Shop' }} />
+      <SearchBar />
+      <CardList />
+    </div>
+  );
+};
 
 export default Home;
