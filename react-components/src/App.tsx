daad06e29a97d7f5726f6c7a142b88306248949c
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-03-29 18:51:27 +0600

    feat: rewrite App class component with function

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -7,21 +7,19 @@ import About from './pages/About/About';
 import NotFound from './pages/NotFound/NotFound';
 import FormPage from './pages/FormPage/FormPage';
 
-class App extends React.Component {
-  render(): React.ReactNode {
-    return (
-      <>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/form" element={<FormPage />} />
-          <Route path="/404" element={<NotFound />} />
-          <Route path="*" element={<Navigate replace to="/404" />} />
-        </Routes>
-      </>
-    );
-  }
-}
+const App: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/form" element={<FormPage />} />
+        <Route path="/404" element={<NotFound />} />
+        <Route path="*" element={<Navigate replace to="/404" />} />
+      </Routes>
+    </>
+  );
+};
 
 export default App;
