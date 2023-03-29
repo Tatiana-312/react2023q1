6d600c15297e32763e455a47214da84502eb9b3c
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-03-29 19:08:09 +0600

    feat: rewrite Card and Cardlist class components with functions

--- react-components/src/components/Card/Card.tsx
+++ react-components/src/components/Card/Card.tsx
@@ -2,23 +2,18 @@ import React from 'react';
 import { CardProps } from './cardProps.interface';
 import classes from './Card.module.css';
 
-class Card extends React.Component<CardProps> {
-  constructor(props: CardProps) {
-    super(props);
-  }
-  render(): React.ReactNode {
-    return (
-      <div className={classes.card} data-testid="card">
-        <div className={classes.card__body}>
-          <img className={classes.card__image} src={this.props.imgUrl} alt="card-image" />
-          <h2 className={classes.card__title}>{this.props.name}</h2>
-          <h3 className={classes.card__subtitle}>{this.props.author}</h3>
-          <p className={classes.card__description}>{this.props.genre}</p>
-        </div>
-        <button className={classes.card__button}>Buy</button>
+const Card: React.FC<CardProps> = ({ imgUrl, name, author, genre }) => {
+  return (
+    <div className={classes.card} data-testid="card">
+      <div className={classes.card__body}>
+        <img className={classes.card__image} src={imgUrl} alt="card-image" />
+        <h2 className={classes.card__title}>{name}</h2>
+        <h3 className={classes.card__subtitle}>{author}</h3>
+        <p className={classes.card__description}>{genre}</p>
       </div>
-    );
-  }
-}
+      <button className={classes.card__button}>Buy</button>
+    </div>
+  );
+};
 
 export default Card;
--- react-components/src/components/CardList/CardList.tsx
+++ react-components/src/components/CardList/CardList.tsx
@@ -4,16 +4,14 @@ import React from 'react';
 import booksData from './booksData.json';
 import classes from './CardList.module.css';
 
-class CardList extends React.Component {
-  render(): React.ReactNode {
-    return (
-      <div className={classes.container}>
-        {booksData.map((data: CardProps) => {
-          return <Card key={data.id} {...data} />;
-        })}
-      </div>
-    );
-  }
-}
+const CardList: React.FC = () => {
+  return (
+    <div className={classes.container}>
+      {booksData.map((data: CardProps) => {
+        return <Card key={data.id} {...data} />;
+      })}
+    </div>
+  );
+};
 
 export default CardList;
