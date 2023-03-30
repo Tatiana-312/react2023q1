5319761af6e3d028bac85ae7a3e8a10e58bfa4a0
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-03-30 15:06:38 +0600

    feat: rewrite DataSaveMessage class component with function

--- react-components/src/components/DataSaveMessage/DataSaveMessage.tsx
+++ react-components/src/components/DataSaveMessage/DataSaveMessage.tsx
@@ -2,17 +2,12 @@ import React from 'react';
 import { DataSaveMessageProps } from './dataSaveMessageProps.interface';
 import classes from './DataSaveMessage.module.css';
 
-class DataSaveMessage extends React.Component<DataSaveMessageProps> {
-  constructor(props: DataSaveMessageProps) {
-    super(props);
-  }
-  render() {
-    return (
-      <>
-        <p className={classes.save_message}>{this.props.dataSaveMessage}</p>
-      </>
-    );
-  }
-}
+const DataSaveMessage: React.FC<DataSaveMessageProps> = ({ dataSaveMessage }) => {
+  return (
+    <>
+      <p className={classes.save_message}>{dataSaveMessage}</p>
+    </>
+  );
+};
 
 export default DataSaveMessage;
