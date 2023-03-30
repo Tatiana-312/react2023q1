e9f9abbfd8ab23d3ceb74a63c6581eef80ce06d2
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-03-30 16:21:31 +0600

    feat: rewrite ValidatorMessage class component with function

--- react-components/src/components/ValidatorErrorMessage/ValidatorMessage.tsx
+++ react-components/src/components/ValidatorErrorMessage/ValidatorMessage.tsx
@@ -2,13 +2,8 @@ import React from 'react';
 import { ValidatorMessageProps } from './validatorMessageProps.interface';
 import classes from './ValidatorMessage.module.css';
 
-class ValidatorMessage extends React.Component<ValidatorMessageProps> {
-  constructor(props: ValidatorMessageProps) {
-    super(props);
-  }
-  render() {
-    return <span className={classes.validation__error}>{this.props.errorText}</span>;
-  }
-}
+const ValidatorMessage: React.FC<ValidatorMessageProps> = ({ errorText }) => {
+  return <span className={classes.validation__error}>{errorText}</span>;
+};
 
 export default ValidatorMessage;
