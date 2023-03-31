251dc23889c53fb4c9c91f270df1c1adaf4da473
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-03-31 22:47:03 +0600

    fix: fix tests

--- react-components/src/components/SearchBar/SearchBar.test.tsx
+++ react-components/src/components/SearchBar/SearchBar.test.tsx
@@ -1,27 +1,23 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 import SearchBar from './SearchBar';
-import { vi } from 'vitest';
-
-const onChange = vi.fn();
-const value = '';
 
 describe('Search component', () => {
   it('renders Search component', () => {
-    render(<SearchBar value={value} onChange={onChange} />);
+    render(<SearchBar />);
 
     expect(screen.getByRole('button')).toBeInTheDocument();
     expect(screen.getByRole('textbox')).toBeInTheDocument();
   });
 
   it('renders with placeholder', () => {
-    render(<SearchBar value={value} onChange={onChange} />);
+    render(<SearchBar />);
 
     expect(screen.getByPlaceholderText('Type text here')).toBeInTheDocument();
   });
 
   it('prints text in textbox', () => {
-    render(<SearchBar value="" onChange={onChange} />);
+    render(<SearchBar />);
     const input = screen.getByPlaceholderText('Type text here');
     fireEvent.change(input, { target: { value: 'React' } });
     expect(input).toHaveValue('React');
