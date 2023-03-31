7c71887af2d87cdd8581c15a983a21356f53ada2
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-03-31 23:32:30 +0600

    feat: add styles for cards

--- react-components/src/components/Card/Card.tsx
+++ react-components/src/components/Card/Card.tsx
@@ -2,16 +2,15 @@ import React from 'react';
 import classes from './Card.module.css';
 import { Data } from '../../pages/Home/data.interface';
 
-const Card: React.FC<Data> = ({ image, name, gender, status }) => {
+const Card: React.FC<Data> = ({ image, name, species }) => {
   return (
     <div className={classes.card} data-testid="card">
       <div className={classes.card__body}>
         <img className={classes.card__image} src={image} alt="card-image" />
         <h2 className={classes.card__title}>{name}</h2>
-        <h3 className={classes.card__subtitle}>{gender}</h3>
-        <p className={classes.card__description}>{status}</p>
+        <h3 className={classes.card__subtitle}>{species}</h3>
       </div>
-      <button className={classes.card__button}>Learn More</button>
+      <button className={classes.card__button}>More</button>
     </div>
   );
 };
