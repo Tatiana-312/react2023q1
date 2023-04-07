494dbc6a2cc745b6e1211519f7d74f568e0dba00
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-04-07 15:11:04 +0600

    feat: mock API

---
+++ react-components/src/pages/Home/mocks/handlers.ts
@@ -0,0 +1,101 @@
+import { rest } from 'msw';
+
+export const handlers = [
+  rest.get('https://rickandmortyapi.com/api/character', (req, res, ctx) => {
+    const param = req.url.searchParams.get('name');
+    if (!param) {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          results: [
+            {
+              id: 1,
+              name: 'Rick Sanchez',
+              status: 'Alive',
+              species: 'Human',
+              type: '',
+              gender: 'Male',
+              location: {
+                name: 'Citadel of Ricks',
+                url: 'https://rickandmortyapi.com/api/location/3',
+              },
+              image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+              url: 'https://rickandmortyapi.com/api/character/1',
+              created: '2017-11-04T18:48:46.250Z',
+            },
+            {
+              id: 2,
+              name: 'Morty Smith',
+              status: 'Alive',
+              species: 'Human',
+              type: '',
+              gender: 'Male',
+              location: {
+                name: 'Citadel of Ricks',
+                url: 'https://rickandmortyapi.com/api/location/3',
+              },
+              image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
+              url: 'https://rickandmortyapi.com/api/character/2',
+              created: '2017-11-04T18:50:21.651Z',
+            },
+          ],
+        })
+      );
+    }
+
+    if (param === 'morty') {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          results: [
+            {
+              id: 2,
+              name: 'Morty Smith',
+              status: 'Alive',
+              species: 'Human',
+              type: '',
+              gender: 'Male',
+              location: {
+                name: 'Citadel of Ricks',
+                url: 'https://rickandmortyapi.com/api/location/3',
+              },
+              image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
+              url: 'https://rickandmortyapi.com/api/character/2',
+              created: '2017-11-04T18:50:21.651Z',
+            },
+          ],
+        })
+      );
+    }
+  }),
+  rest.get('https://rickandmortyapi.com/api/character/1', (_req, res, ctx) => {
+    return res(
+        ctx.status(200),
+        ctx.json({
+            id: 1,
+              name: 'Rick Sanchez',
+              status: 'Alive',
+              species: 'Human',
+              type: '',
+              gender: 'Male',
+              location: {
+                name: 'Citadel of Ricks',
+                url: 'https://rickandmortyapi.com/api/location/3',
+              },
+              image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+              url: 'https://rickandmortyapi.com/api/character/1',
+              created: '2017-11-04T18:48:46.250Z',
+        })
+    )
+  }),
+  rest.get('https://rickandmortyapi.com/api/character/', (req, res, ctx) => {
+    const param = req.url.searchParams.get('name');
+    if (param === 'q4q4')
+    return res(
+        ctx.status(404),
+        ctx.json({
+            errorMessage: 'Could not fetch the data!',
+          }),
+    )
+  }),
+];
