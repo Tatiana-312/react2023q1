b859683fda7a7a98370b29832c755d1267f33442
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-04-12 17:05:01 +0600

    feat: change home page context

--- react-components/src/pages/Home/HomePageContext.tsx
+++ react-components/src/pages/Home/HomePageContext.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { HomePageContextType } from './homePageContext.interface';
 
 export const HomePageContext = React.createContext<HomePageContextType>({
-  setIsLoaded: () => {},
-  setIsError: () => {},
   openModal: () => {},
+  cardId: 0,
 });
--- react-components/src/pages/Home/homePageContext.interface.ts
+++ react-components/src/pages/Home/homePageContext.interface.ts
@@ -1,5 +1,4 @@
 export interface HomePageContextType {
-  setIsLoaded: React.Dispatch<React.SetStateAction<boolean>>;
-  setIsError: React.Dispatch<React.SetStateAction<boolean>>;
   openModal: (e: React.MouseEvent<HTMLElement>) => void;
+  cardId: number;
 }
