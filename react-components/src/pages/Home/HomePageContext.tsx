ce4a555c2d07bc80bba6909abaee40266eefbd8c
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-04-12 20:26:57 +0600

    refactor: delete redundant files

--- react-components/src/pages/Home/HomePageContext.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { HomePageContextType } from './homePageContext.interface';
-
-export const HomePageContext = React.createContext<HomePageContextType>({
-  openModal: () => {},
-  cardId: 0,
-});
--- react-components/src/pages/Home/homePageContext.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface HomePageContextType {
-  openModal: (e: React.MouseEvent<HTMLElement>) => void;
-  cardId: number;
-}
