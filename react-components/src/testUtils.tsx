6020b94ff9784eae43587edc00c2e02c2ceee4b9
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-04-13 11:37:26 +0600

    feat: add testUtils for rendering with redux provider

---
+++ react-components/src/testUtils.tsx
@@ -0,0 +1,56 @@
+import { PreloadedState, combineReducers, configureStore } from '@reduxjs/toolkit';
+import { rickAndMortyApi } from './redux/rickAndMortyApi';
+import { RenderOptions, render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { PropsWithChildren } from 'react';
+import React from 'react';
+import searchValueReducer from './redux/store/searchValueSlice';
+import formCardsDataReducer from './redux/store/formCardsDataSlice';
+import modalReducer from './redux/store/modalSlice';
+
+const rootReducer = combineReducers({
+  searchValue: searchValueReducer,
+  formCardsData: formCardsDataReducer,
+  modal: modalReducer,
+  [rickAndMortyApi.reducerPath]: rickAndMortyApi.reducer,
+});
+
+export const setupStore = (preloadedState?: PreloadedState<RootState>) =>
+  configureStore({
+    reducer: rootReducer,
+    preloadedState,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(rickAndMortyApi.middleware),
+  });
+
+type RootState = ReturnType<typeof rootReducer>;
+type AppStore = ReturnType<typeof setupStore>;
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
+  preloadedState?: PreloadedState<RootState>;
+  store?: AppStore;
+}
+
+export function renderWithProviders(
+  ui: React.ReactElement,
+  {
+    preloadedState = {},
+    store = configureStore({
+      reducer: {
+        searchValue: searchValueReducer,
+        formCardsData: formCardsDataReducer,
+        modal: modalReducer,
+        [rickAndMortyApi.reducerPath]: rickAndMortyApi.reducer,
+      },
+      preloadedState,
+      middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(rickAndMortyApi.middleware),
+    }),
+    ...renderOptions
+  }: ExtendedRenderOptions = {}
+) {
+  function Wrapper({ children }: PropsWithChildren<{}>): JSX.Element {
+    return <Provider store={store}>{children}</Provider>;
+  }
+
+  return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
+}
