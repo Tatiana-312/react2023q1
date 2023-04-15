51c1c053b51ffe5e62fb2bdc70659d2da344cefa
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-04-15 22:30:22 +0600

    refactor: refactor testUtils

--- react-components/src/testUtils.tsx
+++ react-components/src/testUtils.tsx
@@ -1,4 +1,4 @@
-import { PreloadedState, combineReducers, configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { rickAndMortyApi } from './redux/rickAndMortyApi';
 import { RenderOptions, render } from '@testing-library/react';
 import { Provider } from 'react-redux';
@@ -7,33 +7,19 @@ import React from 'react';
 import searchValueReducer from './redux/store/searchValueSlice';
 import formCardsDataReducer from './redux/store/formCardsDataSlice';
 import modalReducer from './redux/store/modalSlice';
+import store from './redux/store/store';
 
-const rootReducer = combineReducers({
-  searchValue: searchValueReducer,
-  formCardsData: formCardsDataReducer,
-  modal: modalReducer,
-  [rickAndMortyApi.reducerPath]: rickAndMortyApi.reducer,
-});
+export const setupStore = () => store;
 
-export const setupStore = (preloadedState?: PreloadedState<RootState>) =>
-  configureStore({
-    reducer: rootReducer,
-    preloadedState,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(rickAndMortyApi.middleware),
-  });
-
-type RootState = ReturnType<typeof rootReducer>;
 type AppStore = ReturnType<typeof setupStore>;
 
 interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
-  preloadedState?: PreloadedState<RootState>;
   store?: AppStore;
 }
 
 export function renderWithProviders(
   ui: React.ReactElement,
   {
-    preloadedState = {},
     store = configureStore({
       reducer: {
         searchValue: searchValueReducer,
@@ -41,7 +27,6 @@ export function renderWithProviders(
         modal: modalReducer,
         [rickAndMortyApi.reducerPath]: rickAndMortyApi.reducer,
       },
-      preloadedState,
       middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(rickAndMortyApi.middleware),
     }),
