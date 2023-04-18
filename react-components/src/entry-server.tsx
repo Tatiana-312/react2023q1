2f8b565b44c7c197f29bfc82a0dda78526d8712f
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-04-18 17:05:20 +0600

    fix: fix entry-server

--- react-components/src/entry-server.tsx
+++ react-components/src/entry-server.tsx
@@ -1,44 +1,17 @@
 import ReactDOMServer from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom/server';
-import type { Request, Response } from 'express';
 import App from './App';
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './redux/store/store';
-import Html from '../Html';
 
-export default function render(url: string, options: any) {
-  const stream = ReactDOMServer.renderToPipeableStream(
+export const render = (url: string, options: object) => {
+  return ReactDOMServer.renderToPipeableStream(
     <StaticRouter location={url}>
       <Provider store={store}>
-        {/* <Html> */}
         <App />
-        {/* </Html> */}
       </Provider>
     </StaticRouter>,
     options
   );
-  return stream;
-}
-
-// export function render(req: Request, res: Response) {
-//   const { pipe } = ReactDOMServer.renderToPipeableStream(
-//     <StaticRouter location={req.originalUrl}>
-//       <Provider store={store}>
-//         {/* <Html> */}
-//         <App />
-//         {/* </Html> */}
-//       </Provider>
-//     </StaticRouter>,
-//     {
-//       onShellReady() {
-//         res.statusCode = 200;
-//         res.setHeader('content-type', 'text/html');
-//         pipe(res);
-//       },
-//       onError(err) {
-//         console.error(err);
-//       },
-//     }
-//   );
-// }
+};
