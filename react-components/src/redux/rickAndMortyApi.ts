c90e2312e6fef4a8a2299f9de42ca90b6e6439b7
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-04-27 15:51:36 +0600

    feat: add custom createApi

--- react-components/src/redux/rickAndMortyApi.ts
+++ react-components/src/redux/rickAndMortyApi.ts
@@ -1,6 +1,12 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { Data } from '../pages/Home/data.interface';
 import { ApiResponse } from './apiResponse.interface';
+import { buildCreateApi, coreModule, reactHooksModule } from '@reduxjs/toolkit/query/react';
+
+const createApi = buildCreateApi(
+  coreModule(),
+  reactHooksModule({ unstable__sideEffectsInRender: true })
+);
 
 export const rickAndMortyApi = createApi({
   reducerPath: 'rickAndMortyApi',
